2f021f4a7768d6ca9539f0c4a6f83be99e197794
PunGrumpy <108584943+PunGrumpy@users.noreply.github.com>
2023-05-20 20:39:26 +0700

    feat: add config from user

--- internal/app/const.go
+++ internal/app/const.go
@@ -1,6 +1,6 @@
 package app
 
 const (
-	Ver  = "2.3.0"
-	Name = "docker-color-output"
+	Ver  = "2.3.1"
+	Name = "dockercolorize"
 )
---
+++ pkg/config/config.go
@@ -0,0 +1,73 @@
+package config
+
+import (
+	"encoding/json"
+	"io/ioutil"
+	"log"
+	"os"
+)
+
+type Config struct {
+	Color ColorConfig `json:"color"`
+}
+
+type ColorConfig struct {
+	Reset       string `json:"reset"`
+	Black       string `json:"black"`
+	DarkGray    string `json:"darkGray"`
+	Red         string `json:"red"`
+	LightRed    string `json:"lightRed"`
+	Green       string `json:"green"`
+	LightGreen  string `json:"lightGreen"`
+	Brown       string `json:"brown"`
+	Yellow      string `json:"yellow"`
+	Blue        string `json:"blue"`
+	LightBlue   string `json:"lightBlue"`
+	Purple      string `json:"purple"`
+	LightPurple string `json:"lightPurple"`
+	Cyan        string `json:"cyan"`
+	LightCyan   string `json:"lightCyan"`
+	LightGray   string `json:"lightGray"`
+	White       string `json:"white"`
+}
+
+var DefaultConfig = Config{
+	Color: ColorConfig{
+		Reset:       "\033[0m",
+		Black:       "\033[0;30m",
+		DarkGray:    "\033[1;30m",
+		Red:         "\033[0;31m",
+		LightRed:    "\033[1;31m",
+		Green:       "\033[0;32m",
+		LightGreen:  "\033[1;32m",
+		Brown:       "\033[0;33m",
+		Yellow:      "\033[1;33m",
+		Blue:        "\033[0;34m",
+		LightBlue:   "\033[1;34m",
+		Purple:      "\033[0;35m",
+		LightPurple: "\033[1;35m",
+		Cyan:        "\033[0;36m",
+		LightCyan:   "\033[1;36m",
+		LightGray:   "\033[0;37m",
+		White:       "\033[1;37m",
+	},
+}
+
+var AppConfig = DefaultConfig
+
+func LoadConfig() {
+	home, _ := os.UserHomeDir()
+	file, err := ioutil.ReadFile(home + "/.config/docker-color-output")
+
+	if err != nil {
+		log.Printf("failed to load color config: %s. Using default.", err)
+		return
+	}
+
+	err = json.Unmarshal(file, &AppConfig)
+
+	if err != nil {
+		log.Printf("failed to parse color config: %s. Using default.", err)
+		AppConfig = DefaultConfig
+	}
+}
